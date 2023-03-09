3eaa6d5e02a71639a64d33130fe69cbe904cf7b3
UserOne <user@gmail.com>
2023-03-09 18:03:37 +0530

    nasa habitable planets

---
+++ index.js
@@ -0,0 +1,31 @@
+const { parse } = require('csv-parse');
+
+const fs = require('fs');
+const result = [];
+
+function isHabitable(planet) {
+  return (
+    planet['koi_prad'] < 1.6 &&
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.1
+  );
+}
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', data => {
+    if (isHabitable(data)) {
+      result.push(data);
+    }
+  })
+  .on('error', err => console.log(err))
+  .on('end', () => {
+    console.log(result.map(result => result['kepler_name']));
+    console.log('done.!');
+  });
